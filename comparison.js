67480c292815df54bcf390acceac9c6ebb35044d
Sami0189 <samisaimon28@gmail.com>
2024-10-25 01:54:27 +0600

    comparison.js

---
+++ comparison.js
@@ -0,0 +1,32 @@
+// console.log(5>10);
+// console.log(5<10);
+// console.log(5==10);
+// console.log(10==10);
+// console.log(10>=10);
+// console.log(15>=10);
+// console.log(5>=10);
+// console.log(5<=10);
+// console.log(15<=10);
+// console.log(10<=10);
+// console.log(10==10.2);
+
+
+// var peyaraPrice = 40;
+// var appleaprice = 250;
+// console.log(peyaraPrice>appleaprice);
+// console.log(peyaraPrice<appleaprice);
+// console.log(peyaraPrice<=appleaprice);
+// console.log(peyaraPrice==appleaprice);
+// console.log(peyaraPrice>=appleaprice);
+// console.log(peyaraPrice<=appleaprice);
+
+
+// console.log(10=="10");
+// console.log(10==="10");
+// console.log(10===10);
+
+
+
+console.log(10!='10');
+console.log(10!=='10');
+console.log(10!=='10');
