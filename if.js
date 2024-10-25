e9b3b3ea5dfccc1fce2d95c0bf6cfaac4b0352f4
Sami0189 <samisaimon28@gmail.com>
2024-10-25 12:02:10 +0600

    if.js

---
+++ if.js
@@ -0,0 +1,31 @@
+// If you buy food for me i will come
+
+// if briyani price less than 300 taka i will come
+
+
+
+// if (condition){
+
+
+
+
+
+// if(3 < 10){
+//     console.log('i have a smaller value');
+// if(13 < 10){
+//     console.log('i have a smaller value');
+// }
+// var briyaniPrice = 250;
+// if(briyaniPrice < 300){
+//     console.log('mama give me some biryani');
+// }
+
+
+
+var wight =20;
+if (wight<=20){
+    console.log('i will carry it by my self');
+}
+else{
+    console.log('i will rent a rickshaw');
+}
