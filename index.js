92784a00365ae4103bef815a3d820ce4645e43fc
Sami0189 <samisaimon28@gmail.com>
2024-10-26 02:27:19 +0600

    index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const numbers = [23,43, 75,452, 645,41];
+ console.log(numbers);
+ console.log(numbers[4]);
+
+ const fourth = numbers[3];
+ console.log(fourth);
+
+
+ //set or update element value by index
+ let abc = 45;
+ abc = 99;
+ numbers[1]=999;
+ console.log(numbers)
