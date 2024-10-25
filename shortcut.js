0adddaedad47a96c482e985c09291838a4f9c7d5
Sami0189 <samisaimon28@gmail.com>
2024-10-25 21:55:33 +0600

    shortcut.js

---
+++ shortcut.js
@@ -0,0 +1,19 @@
+// const isLeader = true;
+
+//isLeader===true same same
+// if (isLeader){    //same same
+//     console.log('leader assa vitora jata dao');
+// }
+// else{
+//     console.log('vitora jata diba na');
+// }
+
+
+const isPassed = false;
+//if(isPassed === false) same same
+if(!isPassed){           //same same
+    console.log('toke biya diya dimo')
+}
+else{
+    console.log('tumi amar')
+}
