e16bbc1bdb9e61a9724757719970fced5c3cf023
Sami0189 <samisaimon28@gmail.com>
2024-10-25 18:21:17 +0600

    multiple.js

---
+++ multiple.js
@@ -0,0 +1,49 @@
+const salary = 23000;
+const isBCS = true;
+const height = 72;
+hascar = false;
+
+// if(salary>20000 && height>66){
+//     console.log('su-----patro');
+// }
+// else{
+//     console.log('onno patro khuji');
+// }
+// if(salary>=23000 || height>71){
+//     console.log('aso baba kobol');
+// }
+// else{
+//     console.log('go to your home');
+// }
+
+
+// if(salary>20000 || height>74 || isBCS == true){
+//     console.log('please, come to my home and marry my girl');
+// }
+// else{
+//     console.log('go to your home');
+// }
+
+
+// if(salary>20000 && height>74 && isBCS == true){
+//     console.log('please, come to my home and marry my girl');
+// }
+// else{
+//     console.log('go to your home');
+// }
+
+
+
+if((salary>22000 && hascar == true) || isBCS == true){
+    console.log('you are my hero');
+}
+else{
+    console.log('again go to your home');
+}
+
+if((salary>22000 || hascar == true) && isBCS == true){
+    console.log('you are my hero');
+}
+else{
+    console.log('again go to your home');
+}
