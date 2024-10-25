3deaed5ba37f5331146089b5bd7fd5037d13bfb5
Sami0189 <samisaimon28@gmail.com>
2024-10-25 19:52:53 +0600

    nested.js

---
+++ nested.js
@@ -0,0 +1,18 @@
+const money = 0;
+
+if (money >300){
+    console.log('bro you are rich');
+}
+else{
+    if (money>100){
+        console.log('tui gorib o na borolok o na')
+    }
+    else{
+        if(money>0){
+            console.log('same same but different');
+        }
+        else{
+            console.log('who are you');
+        }
+    }
+}
