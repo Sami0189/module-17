11fb620c08899ff02d74b32052338c2742b794cd
Sami0189 <samisaimon28@gmail.com>
2024-10-25 21:31:23 +0600

    ternary

---
+++ ternary.js
@@ -0,0 +1,41 @@
+/**
+ * ternary--> three parts
+ *
+ *
+ *
+ * ?   :
+ *
+ * condition ? do something when true : do something when false
+ */
+
+//const age = 20;
+// if(age>18){
+//     console.log('you can vote');
+// }
+// else{
+//     console.log('gumai thako');
+// }
+
+// above items short cut
+
+//age >= 18 ? console.log('vote diyo') : console.log('gumai thako')
+
+let price = 500;
+const isLeader = false;
+
+// if (isLeader === true) {
+//   price = 0;
+// } else {
+//   price = price + 100;
+// }
+// console.log(price);
+
+price = isLeader === true ? 0 : price + 100;
+
+console.log(price)
+
+// if (3 === "3") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
