f89c0eb63277f11e61858196503913002ceb3493
Sami0189 <samisaimon28@gmail.com>
2024-10-26 18:10:10 +0600

    is ayyay.js

---
+++ isarray.js
@@ -0,0 +1,9 @@
+const friends = ['balam','salam','kalam','koilam','gelam'];
+const nums = []
+
+const food = 'ros a gol l a';
+const age = 15;
+console.log(Array.isArray(friends));
+console.log(Array.isArray(nums));
+console.log(Array.isArray(food));
+console.log(Array.isArray(age));
