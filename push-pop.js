0879da7f6b6fa4bd127613f3ff083414f26117d7
Sami0189 <samisaimon28@gmail.com>
2024-10-26 12:16:22 +0600

    push-pop.js

---
+++ push-pop.js
@@ -0,0 +1,18 @@
+const numbers = [12,43,654,54,65,765,34,75,54];
+
+console.log(numbers);
+numbers.push(44);
+console.log(numbers);
+numbers.push(64);
+numbers.push(97,123,769);
+console.log(numbers);
+
+
+const friends = ['balam','kalam','salam'];
+friends.push('ashik','alvi');
+friends.push('sifat',33,654,243,'koi');
+console.log(friends);
+friends.pop();
+friends.pop();
+
+console.log(friends);
