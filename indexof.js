4444670994d67a81590323d6b59b4e7c90c64c64
Sami0189 <samisaimon28@gmail.com>
2024-10-26 13:02:46 +0600

    indexof.js

---
+++ indexof.js
@@ -0,0 +1,6 @@
+const friends = ['balam','salam','kalam','koilam','gelam'];
+
+
+console.log(friends.indexOf('gelam'));
+console.log(friends.indexOf('talam'));
+console.log(friends.indexOf('alam'));
